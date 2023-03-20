4a2d01e5a2d3282119f808af6d20e736cc800e91
thedarkness <thedarkness@thedarknesss-MacBook-Pro.local>
2023-03-20 10:25:36 -0700

    Made a header - awe yeah

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-
+import Header from "./Header";
 
 
 function App(){
   return (
-    <header>
-      <h1>Howdy from App.js</h1>
-    </header>
+    <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(){
+    return (
+        <header>
+            <h1>Howdy from the real header</h1>
+        </header>
+    );
+}
+
+export default Header;
